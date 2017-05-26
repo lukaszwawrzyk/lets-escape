64bf71c6a4ab704707f7a08df5ea3dfd1d426eea
Krzysztof Papciak <papciakk@student.agh.edu.pl>
2017-05-26 02:39:57 +0200

    doors and buttons

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    [SerializeField]
+    private Door target;
+
+    public bool click = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var distance = (Camera.main.transform.position - target.transform.position).magnitude;
+            if(distance < 5.0)
+            {
+                target.Clicked();
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, ButtonTarget {
+
+    [SerializeField]
+    private float angle = 0;
+
+    [SerializeField]
+    private float openAngle = 130.0f;
+
+    [SerializeField]
+    private float openingSpeed = 150.0f;
+
+    private bool opening = false;
+    private bool closing = false;
+
+    private bool opened = false;
+
+    void Update () {
+        if (opening)
+        {
+            angle += Time.deltaTime * openingSpeed;
+
+            if (angle >= openAngle)
+            { 
+                opening = false;
+                angle = openAngle;
+            }
+        }
+        else if (closing)
+        {
+            angle -= Time.deltaTime * openingSpeed;
+
+            if (angle <= 0)
+            {
+                closing = false;
+                angle = 0;
+            }
+        }
+
+        transform.localRotation = Quaternion.Euler(new Vector3(0, angle, 0));
+    }
+
+    void Open()
+    {
+        opened = true;
+        opening = true;
+    }
+
+    void Close()
+    {
+        opened = false;
+        closing = true;
+    }
+
+    public void Clicked()
+    {
+        if (opened)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+        
+    }
+}
